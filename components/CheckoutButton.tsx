651cbd402aa24ad079591c079c9eb600ccaad4ad
graslowsnail <graslowsnail@gmail.com>
2023-09-27 22:58:31 -0500

    redploy2

--- components/CheckoutButton.tsx
+++ components/CheckoutButton.tsx
@@ -2,7 +2,7 @@
 import React, { useEffect } from 'react';
 import { loadStripe } from '@stripe/stripe-js';
 
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
 
 export default function PreviewPage() {
   useEffect(() => {
