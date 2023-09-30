e7c9f0253237819dabb4ccf380d50ca420555d42
graslowsnail <graslowsnail@gmail.com>
2023-09-30 05:07:24 -0500

    changed footer

--- constants/index.tsx
+++ constants/index.tsx
@@ -3,7 +3,7 @@ export const footerLinks = [
     title: "Socials",
     links: [
       { title: "Instagram", url: "https://www.instagram.com/graslowsnail/" },
-      { title: "X", url: "https://twitter.com/graslowsnail" },
+      { title: "Twitter/X", url: "https://twitter.com/graslowsnail" },
       { title: "GitHub", url: "https://github.com/graslowsnail" },
     ],
   },
