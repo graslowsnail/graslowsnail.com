61d905634fe799ca9bfccc229da2f74d5abdd66b
graslowsnail <graslowsnail@gmail.com>
2024-12-02 18:27:27 -0600

    changed sentence in about me

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -21,7 +21,7 @@ export default function About() {
           {/* Displaying additional picture information */}
           <p className="text-4xl mb-1 font-regular font-['Koulen']">ABOUT ME</p>
 
-         <p className="text-md font-light mb-2"> Born and raised in Houston, I've discoverd the art of analog photography over the last two years.   </p>
+         <p className="text-md font-light mb-2"> Born and raised in Houston, I've discoverd the art of analog photography over the years.   </p>
            <p className="text-md font-light mb-2">This journey has allowed me to capture moments from my life, which I'm excited to share with you! </p>
            <p className="text-md font-light mb-4"> I created this website not only as a platform to showcase my work but also to offer these captured memories for purchase, allowing them to be displayed and appreciated by a wider audience.</p>
            <p className="text-md font-light mb-2"> Have any questions? Dont be afraid to reach out!</p>
