34c6997601b0fffcc1f0c041dca007f077b9ce17
graslowsnail <graslowsnail@gmail.com>
2025-05-12 00:31:31 -0500

    blogs in desending id order

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,13 +2,16 @@ import Blog from "@/components/Blog";
 import { sampleBlogs } from "@/constants";
 
 export default function BlogPage() {
+  // Sort blogs by descending ID
+  const sortedBlogs = [...sampleBlogs].sort((a, b) => b.id - a.id);
+
   return (
     <main className="min-h-screen pt-24 px-4 sm:px-16">
       <h1 className="text-4xl font-normal text-center mb-12 font-['Koulen']">
         BLOG
       </h1>
       <div className="max-w-4xl mx-auto space-y-6">
-        {sampleBlogs.map((blog) => (
+        {sortedBlogs.map((blog) => (
           <Blog
             key={blog.id}
             id={blog.id}
